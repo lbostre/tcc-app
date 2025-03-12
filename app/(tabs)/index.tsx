4cc862a386b56716af2ba38fa36adb9d1fb3a1d6
Leyton <leytonbostre@gmail.com>
2025-03-12 15:22:40 -0400

    fix map formatting

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -215,7 +215,7 @@ const styles = StyleSheet.create({
     },
     mapContainer: {
         width: "100%",
-        height: "100%",
+        height: "75%",
     },
     loadingContainer: {
         flex: 1,
